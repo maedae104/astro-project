80e126773dc4291c79dc93b0423dd9b7a0a6add2
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-04-12 15:57:07 -0700

    working on moon phase web scrape

--- web-scrape.py
+++ web-scrape.py
@@ -1,14 +1,15 @@
-import requests
+import requests, json
 from bs4 import BeautifulSoup
 
 URL = "https://mooncalendar.astro-seek.com/"
 page = requests.get(URL)
 
-print(page.text)
+# print(page.text)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
+table = soup.find(id="tabs_content_container")
+dates = table.find_all("div", class_="tab_content")
 
-
-# results = soup.find(name="Date")
-# dates = results.find_all("div", class_="card-content")
+for table_element in dates:
+    print(table_element)
