5152b07ff9908089b4e7d96490ed1119883ccd10
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-04-12 18:10:25 -0700

    created rows variable with html in it

--- web-scrape.py
+++ web-scrape.py
@@ -4,12 +4,12 @@ from bs4 import BeautifulSoup
 URL = "https://mooncalendar.astro-seek.com/"
 page = requests.get(URL)
 
-# print(page.text)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
 table = soup.find(id="tabs_content_container")
-dates = table.find_all("div", class_="tab_content")
+rows = table.find_all("tr", class_="ruka")
 
-for table_element in dates:
-    print(table_element)
+for row in rows:
+    print(row.prettify())
+
