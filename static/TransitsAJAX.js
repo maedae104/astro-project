c0466984dcc8b91511949ed359ae58b07cc03586
Mae Dae <maedae@Maes-MacBook-Air.local>
2022-05-12 20:11:55 -0700

    styling added pictures to divs

--- static/TransitsAJAX.js
+++ static/TransitsAJAX.js
@@ -2,7 +2,9 @@ const aspect_btn = document.querySelector("#view-more")
 
 const eclipse_div = document.getElementById("eclipse-id")
 
-const aspects_div = document.getElementById("aspects-id")
+const aspects_sun_div = document.getElementById("aspects-sun")
+
+const aspects_moon_div = document.getElementById("aspects-moon")
 
 const retro_div = document.getElementById("retro-id")
 
@@ -10,12 +12,14 @@ const retro_div = document.getElementById("retro-id")
 aspect_btn.addEventListener('click', evt => {
         if(evt.target.value == 'View More'){
                 eclipse_div.style.display = 'block'
-                aspects_div.style.display = 'block'
+                aspects_moon_div.style.display = 'block'
+                aspects_sun_div.style.display = 'block'
                 retro_div.style.display = 'block'
 
                 } else if(evt.target.value == 'View Less'){
                 eclipse_div.style.display = 'none'
-                aspects_div.style.display = 'none'
+                aspects_moon_div.style.display = 'none'
+                aspects_sun_div.style.display = 'none'
                 retro_div.style.display = 'none'
                 }
         });
